7503e88fe95e4e4b51dd148d41b7d1d4360b181b
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2024-06-17 07:02:23 +0300

    feat: upload components SubscribeSection

---
+++ src/components/SubscribeSection/SubscribeSection.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import subscribeIcon from "../../assets/img/sendEmail.svg";
+import "./styles/SubscribeSection.scss";
+
+const SubscribeSection: React.FC = () => {
+  const handleSubscribe = () => {
+    alert("Подписка оформлена!");
+  };
+
+  return (
+    <section className="subscription-section">
+      <h2 className="subscription-section__title1">Support</h2>
+      <h2 className="subscription-section__title2">
+        Subscribe Newsletter & get
+      </h2>
+      <h3 className="subscription-section__title3">Bank News</h3>
+      <div className="subscription-section__form">
+        <input
+          type="email"
+          placeholder="Your email"
+          className="subscription-section__input"
+        />
+        <button
+          className="subscription-section__button"
+          onClick={handleSubscribe}
+        >
+          <img
+            src={String(subscribeIcon)}
+            alt="Subscribe"
+            className="subscription-section__icon"
+          />
+          Subscribe
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default SubscribeSection;
