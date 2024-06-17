dc546783daf20f57ca3461e2bff7642525757f04
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2024-06-17 07:06:10 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./styles/index.scss";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement,
-// );
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
