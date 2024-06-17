e2f2118de5305687a90bfff3ce9b953e60e5aa46
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2024-06-17 06:59:17 +0300

    feat: update components FeatureSection

---
+++ src/components/FeatureSection/FeatureSection.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { featuresList } from "./constants";
+import Illustration from "../../assets/img/Illustration2.svg";
+import CheckIcon from "../../assets/img/bx_bxs-check-circle.svg";
+import "./styles/FeatureSection.scss";
+
+const FeatureSection: React.FC = () => {
+  return (
+    <section className="feature-section">
+      <div className="feature-section__image">
+        <img src={String(Illustration)} alt="Illustration" />
+      </div>
+      <div className="feature-section__content">
+        <h2 className="feature-section__title">
+          We Provide Many Features You Can Use
+        </h2>
+        <p className="feature-section__description">
+          You can explore the features that we provide with fun and have their
+          own functions each feature
+        </p>
+        <ul className="feature-section__list">
+          {featuresList.map((feature) => (
+            <li key={feature} className="feature-section__listItem">
+              <img
+                src={String(CheckIcon)}
+                alt="Check Icon"
+                className="feature-section__listIcon"
+              />
+              {feature}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureSection;
---
+++ src/components/FeatureSection/constants.ts
@@ -0,0 +1,6 @@
+export const featuresList = [
+  "Powerful online protection.",
+  "Cashback without borders.",
+  "Personal design.",
+  "Work anywhere in the world.",
+];
