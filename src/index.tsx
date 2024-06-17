02aea3f820f8842225c2396696e53a80da5a0e4a
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2024-06-17 07:05:26 +0300

    feat: upload index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./styles/index.scss";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement,
+// );
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.scss";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
