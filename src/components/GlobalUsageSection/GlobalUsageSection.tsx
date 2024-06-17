e4ce07c9b3627db0051d2f5bf73753e34e1546fa
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2024-06-17 07:00:17 +0300

    feat: update components GlobalUsageSection

---
+++ src/components/GlobalUsageSection/GlobalUsageSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import GlobalImage from "../../assets/img/HugeGlobal.svg";
+import "./styles/GlobalUsageSection.scss";
+
+const GlobalUsageSection: React.FC = () => {
+  return (
+    <section className="global-services">
+      <h2 className="global-services__title">
+        You can use our services anywhere in the world
+      </h2>
+      <p className="global-services__text">
+        Withdraw and transfer money online through our application
+      </p>
+      <div className="global-services__image">
+        <img src={String(GlobalImage)} alt="Global Services" />
+      </div>
+    </section>
+  );
+};
+
+export default GlobalUsageSection;
