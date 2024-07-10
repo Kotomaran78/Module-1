7c8109c90b79d10c8b63bbc43186c5aa5884589e
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-11 00:15:52 +0300

    fix: разделение компонентов по папкам

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,33 @@
-const path = require("path");
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export default {
+//   webpack: {
+//     alias: {
+//       "@components": path.resolve(__dirname, "src/components/"),
+//       "@pages": path.resolve(__dirname, "src/pages/"),
+//       "@styles": path.resolve(__dirname, "src/styles/"),
+//       "@assets": path.resolve(__dirname, "src/assets/")
+//     }
+//   }
+// };
+
+
+
+const path = require('path');
+
 module.exports = {
   webpack: {
     alias: {
-      '@components': path.resolve(__dirname, "src/components/"),
-      '@pages': path.resolve(__dirname, "src/pages/"),
-      '@styles': path.resolve(__dirname, "src/styles/"),
-      '@assets': path.resolve(__dirname, "src/assets/")
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@assets': path.resolve(__dirname, 'src/assets/')
     }
   }
-}
+};
+
