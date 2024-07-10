6fe1df50cc2753c42700f7706415b553d5e5c961
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-10 23:22:34 +0300

    feat(subscribe): добавлена проверка подписки и функциональность отправки email

--- src/components/SubscribeSection/SubscribeSection.tsx
+++ src/components/SubscribeSection/SubscribeSection.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import subscribeIcon from '@assets/icons/sendEmail.svg';
 import './SubscribeSection.scss';
 
 const SubscribeSection: React.FC = () => {
-  const handleSubscribe = () => {
-    alert('Подписка оформлена!');
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const subscribed = localStorage.getItem('isSubscribed');
+    if (subscribed) {
+      setIsSubscribed(true);
+    }
+  }, []);
+
+  const handleSubscribe = async () => {
+    try {
+      const response = await fetch('/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setIsSubscribed(true);
+        localStorage.setItem('isSubscribed', 'true');
+      } else {
+        alert('Не удалось оформить подписку.');
+      }
+    } catch (error) {
+      console.error('Ошибка при подписке:', error);
+    }
   };
 
   return (
@@ -12,13 +39,30 @@ const SubscribeSection: React.FC = () => {
       <h2 className='subscription-section__title1'>Support</h2>
       <h2 className='subscription-section__title2'>Subscribe Newsletter & get</h2>
       <h3 className='subscription-section__title3'>Bank News</h3>
-      <div className='subscription-section__form'>
-        <input type='email' placeholder='Your email' className='subscription-section__input' />
-        <button className='subscription-section__button' onClick={handleSubscribe}>
-          <img src={String(subscribeIcon)} alt='Subscribe' className='subscription-section__icon' />
-          Subscribe
-        </button>
-      </div>
+      {isSubscribed ? (
+        <h3 className='subscription-section__title3'>
+          You are already subscribed to the bank&apos;s newsletter
+        </h3>
+      ) : (
+        <form className='subscription-section__form' onSubmit={handleSubscribe}>
+          <input
+            type='email'
+            placeholder='Your email'
+            className='subscription-section__input'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button type='submit' className='subscription-section__button'>
+            <img
+              src={String(subscribeIcon)}
+              alt='Subscribe'
+              className='subscription-section__icon'
+            />
+            Subscribe
+          </button>
+        </form>
+      )}
     </section>
   );
 };
