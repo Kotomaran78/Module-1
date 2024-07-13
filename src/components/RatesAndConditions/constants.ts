6c1e7a134e0812401be40734a3eac0d661766c97
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-13 17:47:11 +0300

    feat(components): добавлен компонент RatesAndConditions

---
+++ src/components/RatesAndConditions/constants.ts
@@ -0,0 +1,16 @@
+export const ratesAndConditionsData = [
+  { title: 'Card currency', detail: 'Rubles, dollars, euro' },
+  { title: 'Interest free period', detail: '0% up to 160 days' },
+  { title: 'Payment system', detail: 'Mastercard, Visa' },
+  { title: 'Maximum credit limit on the card', detail: '600 000 ₽' },
+  {
+    title: 'Replenishment and withdrawal',
+    detail: 'At any ATM. Top up your credit card for free with cash or transfer from other cards',
+  },
+  { title: 'Max cashback per month', detail: '15 000 ₽' },
+  {
+    title: 'Transaction Alert',
+    detail:
+      '60 ₽ — SMS or push notifications\n0 ₽ — card statement, information about transactions in the online bank',
+  },
+];
