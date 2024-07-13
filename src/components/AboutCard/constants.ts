d56209611bec8bbaad83202fdbf26b5884c1639e
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-13 17:45:17 +0300

    feat(components): добавлен компонент AboutCard

---
+++ src/components/AboutCard/constants.ts
@@ -0,0 +1,34 @@
+import Money_duotone from '@assets/icons/Money_duotone.svg';
+import Calendar_duotone from '@assets/icons/Calendar_duotone.svg';
+import Clock_duotone from '@assets/icons/Clock_duotone.svg';
+import Bag_duotone from '@assets/icons/Bag_duotone.svg';
+import card_duotone from '@assets/icons/Credit_card_duotone.svg'
+
+export const aboutCardData = [
+  {
+    description: 'Cash and transfers without commission and percent',
+    detail: 'Up to 50 000 ₽',
+    icon: Money_duotone,
+  },
+  {
+    description: 'Without percent on the loan',
+    detail: 'Up to 160 days',
+    icon: Calendar_duotone,
+  },
+  {
+    description: 'We will deliver your card by courier at a convenient place and time for you',
+    detail: 'Free delivery',
+    icon: Clock_duotone,
+  },
+  {
+    description: 'No percent. For equipment, clothes and other purchases in installments',
+    detail: 'Up to 12 months',
+    icon: Bag_duotone,
+  },
+  {
+    description:
+      'At any ATM. Top up your credit card for free with cash or transfer from other cards',
+    detail: 'Convenient deposit and withdrawal',
+    icon: card_duotone,
+  },
+];
