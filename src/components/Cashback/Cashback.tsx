77c5016d9069e03ca4ace422905c2b30be2462e8
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-13 17:46:23 +0300

    feat(components): добавлен компонент Cashback

---
+++ src/components/Cashback/Cashback.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { cashbackData } from './constants';
+import './Cashback.scss';
+
+const Cashback: React.FC = () => (
+  <div className="cashback">
+    {cashbackData.map((item) => (
+      <div key={item.description} className="cashback__item">
+        <p>{item.description}</p>
+        <strong>{item.percentage}</strong>
+      </div>
+    ))}
+  </div>
+);
+
+export default Cashback;
