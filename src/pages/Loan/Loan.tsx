1ff9b502bf55ef30bd27d5c011f06030f0724f58
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-14 22:30:56 +0300

    add(components): добавлен новый компонент

--- src/pages/Loan/Loan.tsx
+++ src/pages/Loan/Loan.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Loan.scss';
 import CreditCardOffer from '@components/CreditCardOffer';
+import HowToGetCard from '@components/HowToGetCard';
+import CreditCardInfo from '@components/CreditCardInfo';
 
 const Loan: React.FC = () => {
   return (
     <div className='loan__content'>
       <CreditCardOffer />
+      <CreditCardInfo />
+      <HowToGetCard />
     </div>
   );
 };
