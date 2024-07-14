f9822dd75d769c634f9720cd022480340bdb04e0
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-14 22:28:18 +0300

    feat: в процессе перехода на последнюю версию eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended"
-  ],
-  rules: {
-    indent: ["error", 2],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "react/jsx-uses-react": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off"
-  }
-};
+// module.exports = {
+//   parser: "@typescript-eslint/parser",
+//   plugins: ["@typescript-eslint", "react"],
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:react/recommended"
+//   ],
+//   rules: {
+//     indent: ["error", 2],
+//     quotes: ["error", "single"],
+//     semi: ["error", "always"],
+//     "react/jsx-uses-react": "off",
+//     "@typescript-eslint/explicit-module-boundary-types": "off"
+//   }
+// };
