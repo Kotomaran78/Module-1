bcc2bb0b04f075b9193543c636e88b13ebf3a28b
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-19 13:48:44 +0300

    delete: удаление пустых файлов

--- src/components/CreditCardOffer/constants.ts
+++
@@ -1,8 +0,0 @@
-// import cardImage1 from '@assets/img/cardImage1.png';
-// import cardImage2 from '@assets/img/cardImage2.png';
-// import cardImage3 from '@assets/img/cardImage3.png';
-// import cardImage4 from '@assets/img/cardImage4.png';
-
-// export const cardImages = [cardImage1, cardImage2, cardImage3, cardImage4];
-
-export {};
