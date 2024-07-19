cf51952b22bbf6fe8f68dbf34eb5ecb6129f4227
Kotomaran78 <dendmisharsh@gmail.com>
2024-07-19 13:23:43 +0300

    fix: убрано дублирование App

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -13,12 +13,6 @@ const router = createBrowserRouter([
         path: '',
         element: <Home />,
       },
-    ],
-  },
-  {
-    path: '/',
-    element: <App />,
-    children: [
       {
         path: 'credit-card',
         element: <Loan />,
